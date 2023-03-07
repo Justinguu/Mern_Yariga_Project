d5ed92451877cef37be3c5ba48bc591908b5ce7a
Justinguu <justinguuu@gmail.com>
2023-03-07 14:26:09 -0500

    pagination, selects,filtering complete

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -2,7 +2,7 @@ import { Add } from "@mui/icons-material";
 import { useTable } from "@pankod/refine-core";
 import { Box, Stack, Typography, TextField, Select, MenuItem } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
-
+import { useMemo } from 'react';
 import { PropertyCard, CustomButton } from "components";
 
 const AllProperties = () => {
@@ -26,6 +26,23 @@ const AllProperties = () => {
   const toggleSort = (field: string) => {
     setSorter([{ field, order: currentPrice === 'asc' ? 'desc' : 'asc'}])
   }
+//useMemo  to keep expensive, resource intensive functions from needlessly running.
+
+// currentFilterValues filters by title
+const currentFilterValues = useMemo(() => {
+  const logicalFilters = filters.flatMap((item) =>
+      "field" in item ? item : [],
+  );
+
+  return {
+      title:
+          logicalFilters.find((item) => item.field === "title")?.value ||
+          "",
+      propertyType:
+          logicalFilters.find((item) => item.field === "propertyType")
+              ?.value || "",
+  };
+}, [filters]);
 
   if (isLoading) return <Typography>Loading...</Typography>;
   if (isError) return <Typography>Error...</Typography>;
@@ -39,11 +56,28 @@ const AllProperties = () => {
           </Typography>
           <Box mb={2} mt={3} display="flex" width="84%" justifyContent="space-between" flexWrap="wrap">
             <Box display="flex" gap={2} flexWrap="wrap" mb={{ xs: "20px", sm: 0 }}>
-              <CustomButton   title={`Sort price ${currentPrice === "asc" ? "↑" : "↓"}`}
-              handleClick={() => toggleSort('price')}
-              backgroundColor="#475be8" 
-              color="#fcfcfc" />
-              <TextField variant="outlined" color="info" placeholder="Search by title" value="" onChange={() => {}} />
+              <CustomButton
+                title={`Sort price ${currentPrice === "asc" ? "↑" : "↓"}`}
+                handleClick={() => toggleSort("price")}
+                backgroundColor="#475be8"
+                color="#fcfcfc"
+              />
+
+              <TextField
+                variant="outlined"
+                color="info"
+                placeholder="Search by title"
+                value={currentFilterValues.title}
+                onChange={(e) => {
+                  setFilters([
+                    {
+                      field: "title",
+                      operator: "contains",
+                      value: e.currentTarget.value ? e.currentTarget.value : undefined,
+                    },
+                  ]);
+                }}
+              />
               <Select
                 variant="outlined"
                 color="info"
@@ -51,10 +85,28 @@ const AllProperties = () => {
                 required
                 inputProps={{ "aria-label": "Without label" }}
                 defaultValue=""
-                value=""
-                onChange={() => {}}
+                value={currentFilterValues.propertyType}
+                onChange={(e) => {
+                  setFilters(
+                    [
+                      {
+                        field: "propertyType",
+                        operator: "eq", // eq = equal
+                        value: e.target.value,
+                      },
+                    ],
+                    "replace"
+                  ); // replace the value
+                }}
               >
                 <MenuItem value="">All</MenuItem>
+                {["Apartment", "Villa", "Farmhouse", "Condos", "Townhouse", "Duplex", "Studio", "Chalet"].map(
+                  (type) => (
+                    <MenuItem key={type} value={type.toLowerCase()}>
+                      {type}
+                    </MenuItem>
+                  )
+                )}
               </Select>
             </Box>
           </Box>
@@ -111,14 +163,15 @@ const AllProperties = () => {
             displayEmpty
             required
             inputProps={{ "aria-label": "Without label" }}
-            defaultValue={10}
+            defaultValue={5}
             value=""
-            onChange={() => {}}
+            onChange={(e) => setPageSize(e.target.value ? Number(e.target.value) : 5)}
           >
-            {[10, 20, 30, 40, 50].map((size) => (
-               <MenuItem key={size} value={size}>Show {size}</MenuItem>
+            {[5, 10, 15, 20, 25].map((size) => (
+              <MenuItem key={size} value={size}>
+                Show {size}
+              </MenuItem>
             ))}
-           
           </Select>
         </Box>
       )}
