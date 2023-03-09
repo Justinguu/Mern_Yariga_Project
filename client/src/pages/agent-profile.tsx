6f8feca72a2d2a1bcf3c9f849521f6250b4acb7e
Justinguu <justinguuu@gmail.com>
2023-03-09 17:39:07 -0500

    took out components

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -7,7 +7,7 @@ const AgentProfile = () => {
     const { id } = useParams();
 
     const { data, isLoading, isError } = useOne({
-        resource: "users",
+        resource: "api/v1/users",
         id: id as string,
     });
 
